bb6acdd79b66a9ce5ba436d8969267f144c6ad36
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2019-09-23 09:41:09 -0400

    undo sensitive

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
 variable "softlayer_username" {
   description = "Enter the user name to access IBM Cloud classic infrastructure. Go to the [IBM Cloud API keys](https://cloud.ibm.com/iam/apikeys) page, and hover on the row of your API key, and select the **Actions** icon. Then, click **Details** and copy your user name. "
-  sensitive   = true
 }
 variable "softlayer_api_key" {
   description = "Enter the API key to access IBM Cloud classic infrastructure. For more information for how to create an API key and retrieve it, see [Managing classic infrastructure API keys](https://cloud.ibm.com/docs/iam?topic=iam-classic_keys)."
-  sensitive   = true
 }
 
 provider "ibm" {
