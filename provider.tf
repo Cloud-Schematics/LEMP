4c224f9797e598234204f9e234a759a727e4260a
hkantare@in.ibm.com <hkantare@in.ibm.com>
2019-09-25 10:09:21 +0530

    Remove default for ssh_key

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ variable "softlayer_api_key" {
 provider "ibm" {
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key = "${var.softlayer_api_key}"
+  softlayer_timeout = 300
 }
 
 
